257c6f2221b6de719f8bb36505a3a207c198a737
Peter Gonščák <peter.gonscak190@gmail.com>
2021-11-20 10:58:50 +0100

    done

--- Open-Lab-04.05/StringTools.cs
+++ Open-Lab-04.05/StringTools.cs
@@ -6,7 +6,17 @@ namespace Open_Lab_04._05
     {
         public string Repeat(string orig, int n)
         {
-            throw new NotImplementedException();
+            string a = "";
+            foreach (char letter in orig)
+            {
+                string b = "";
+                for (int i = 0; i < n; i++)
+                {
+                    b += letter;
+                }
+                a += b;
+            }
+            return a;
         }
     }
 }
